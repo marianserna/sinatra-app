2b69788ec186b6a4882829e0ee0165a9356c2a31
Marian Serna <marianhalliday@gmail.com>
2017-10-03 15:26:19 -0400

    Creating favourites route + view and outputting links within view

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
@@ -8,8 +12,23 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = [
+    'http://www.jam3.com/',
+    'https://activetheory.net/',
+    'http://cloudsovercuba.com/',
+    'http://residente.com/en/',
+    'http://www.elcolombiano.com/'
+  ]
+  erb :favourites
 end
